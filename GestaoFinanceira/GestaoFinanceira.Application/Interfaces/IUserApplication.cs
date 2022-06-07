bb05ca77827081477d3e34e1627b41ea96f37751
Diogo Feital <diiogofeital@gmail.com>
2022-06-06 21:22:06 -0300

    First Commit.

---
+++ GestaoFinanceira/GestaoFinanceira.Application/Interfaces/IUserApplication.cs
@@ -0,0 +1,12 @@
+﻿using GestaoFinanceira.Domain.Entities;
+
+namespace GestaoFinanceira.Application.Interfaces;
+
+public interface IUserApplication
+{
+    #region Public Methods
+
+    Task<IEnumerable<User?>> GetAll();
+
+    #endregion Public Methods
+}
---
+++ GestaoFinanceira/GestaoFinanceira.Service.Api/Program.cs
@@ -0,0 +1,36 @@
+using GestaoFinanceira.Infra.Context;
+using GestaoFinanceira.Infra.CrossCutting.Ioc;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<GestaoFinanceiraContext>(options =>
+{
+    options.UseMySQL("server=localhost;database=GestaoFinanceiraDB;user=admin;password=admin;");
+});
+
+builder.Services.ResolveDependencies();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
